378c1c82761518af57bd7d055facf46203185c04
Juan Pablo Orsay <jporsay@gmail.com>
2012-12-19 11:05:51 -0300

    moved implementation to header file

--- src/locator/Locator.cpp
+++ src/locator/Locator.cpp
@@ -3,28 +3,9 @@
 namespace locator {
 
 template<class I, class N>
-I* Locator<I, N>::service = 0;
+I* Locator<I, N>::service;
 
 template<class I, class N>
-N Locator<I, N>::nullService = N();
-
-template<class I, class N>
-void Locator<I, N>::initialize() {
-    service = &nullService;
-}
-
-template<class I, class N>
-void Locator<I, N>::provide(I* newService) {
-    if (newService == 0) {
-        initialize();
-    } else {
-        service = newService;
-    }
-}
-
-template<class I, class N>
-Locator<I,N>::~Locator() {
-    delete service;
-}
+N Locator<I, N>::nullService;
 
 }
--- src/locator/Locator.hpp
+++ src/locator/Locator.hpp
@@ -1,12 +1,25 @@
+#pragma once
 namespace locator {
 
 template <class I, class N>
 class Locator {
 public:
-    static void initialize();
-    static void provide(I* newService);
-    static I& get() { return *service; }
-    virtual ~Locator();
+    static void initialize() {
+        service = &nullService;
+    }
+    static void provide(I* newService) {
+        if (newService == 0) {
+            initialize();
+        } else {
+            service = newService;
+        }
+    }
+    static I& get() {
+        return *service;
+    }
+    virtual ~Locator() {
+        delete service;
+    }
 private:
     Locator();
     static I* service;
