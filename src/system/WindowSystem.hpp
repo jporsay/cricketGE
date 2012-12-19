905e74872aebc9ca60ae6654927896a5c05ec117
Juan Pablo Orsay <jporsay@gmail.com>
2012-12-19 01:50:55 -0300

    removed unnecessary code

--- src/system/WindowSystem.hpp
+++ src/system/WindowSystem.hpp
@@ -6,12 +6,9 @@ namespace system {
 class EventSystem;
 
 class WindowSystem : public System {
-private:
-
 public:
     virtual bool isOpen() = 0;
     virtual void clear() = 0;
-    virtual void registerEventHandler(EventSystem& system);
     virtual ~WindowSystem() {}
 };
 
