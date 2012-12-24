b78c172b7a6a980764d6951d7e2a306952ef0cfc
Juan Pablo Orsay <jporsay@gmail.com>
2012-12-24 14:33:02 -0300

    conveted Window into an interface

--- src/cricket/window/Window.hpp
+++ src/cricket/window/Window.hpp
@@ -1,21 +1,43 @@
 #pragma once
-#include <SFML/Graphics.hpp>
+
+#include <string>
+
 #include "../event/Event.hpp"
 
 namespace window {
 
 class Window {
 private:
-    sf::RenderWindow* _window;
+    int width;
+    int height;
+    std::string title;
 public:
-    Window(int width, int height, std::string title);
-    ~Window();
-    bool isOpen() { return _window->isOpen(); }
-    bool handleEvents();
-    event::Event *getEvent();
-    void clear();
-    void display();
-    void close();
+    Window(int w, int h, std::string t) : 
+        width(w),
+        height(h),
+        title(t)
+    {}
+
+    virtual bool isOpen() const = 0;
+    virtual const event::Event *getEvent() const = 0;
+    virtual void clear() = 0;
+    virtual void display() = 0;
+    virtual void close() = 0;
+    virtual void pumpEvents() = 0;
+
+    const int getWidth() const {
+        return width;
+    }
+
+    const int getHeight() const {
+        return height;
+    }
+
+    const std::string getTitle() const {
+        return title;
+    }
+
+    virtual ~Window() {}
 };
 
 }
