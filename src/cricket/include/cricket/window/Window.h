c71b53746be10ce12923355396f9b7c60e94af59
Juan Pablo Orsay <jporsay@gmail.com>
2012-12-25 12:43:13 -0300

    centralized headers in include/cricket directory

---
+++ src/cricket/include/cricket/window/Window.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+
+#include "../event/EventBuilder.h"
+
+namespace window {
+
+template <class T>
+class Window {
+private:
+    int width;
+    int height;
+    std::string title;
+    event::EventBuilder<T>* eventBuilder;
+public:
+    Window(int w, int h, std::string t) : 
+        width(w),
+        height(h),
+        title(t),
+        eventBuilder(0)
+    {}
+
+    virtual bool initialize() = 0;
+    virtual bool isOpen() const = 0;
+    virtual const event::Event *getEvent() const = 0;
+    virtual void clear() = 0;
+    virtual void display() = 0;
+    virtual void close() = 0;
+    virtual void pumpEvents() = 0;
+
+    void setEventBuilder(event::EventBuilder<T> *builder) {
+        eventBuilder = builder;
+    }
+
+    const event::EventBuilder<T>* getEventBuilder() const {
+        return eventBuilder;
+    }
+
+    const int getWidth() const {
+        return width;
+    }
+
+    const int getHeight() const {
+        return height;
+    }
+
+    const std::string getTitle() const {
+        return title;
+    }
+
+    virtual ~Window() {}
+};
+
+}
