73901c2a520e11b7d2e33ea6fe669d1a6cfb1322
Juan Pablo Orsay <jporsay@gmail.com>
2012-12-24 21:21:03 -0300

    started working on event buildeR

--- src/cricket/window/SFMLEventBuilder.cpp
+++ src/cricket/window/SFMLEventBuilder.cpp
@@ -1,23 +1,33 @@
 #include "SFMLEventBuilder.hpp"
+#include "../event/ClosedEvent.hpp"
+#include "../event/KeyboardEvent.hpp"
+#include "../keyboard/Keys.hpp"
 
 namespace event {
 
-const Event& SFMLEventBuilder::build(sf::Event* rawEvent) {
+const Event& SFMLEventBuilder::build(sf::Event* e) {
     Event* event;
-    switch (rawEvent->type) {
+    switch (e->type) {
         case sf::Event::Closed:
+            event = this->buildClosedEvent(e);
             break;
         case sf::Event::Resized:
+            event = this->buildResizedEvent(e);
             break;
         case sf::Event::KeyPressed:
+            event = this->buildKeyPressedEvent(e);
             break;
         case sf::Event::KeyReleased:
+            event = this->buildKeyReleasedEvent(e);
             break;
         case sf::Event::MouseMoved:
+            event = this->buildMouseMovedEvent(e);
             break;
         case sf::Event::MouseButtonPressed:
+            event = this->buildMouseButtonPressedEvent(e);
             break;
         case sf::Event::MouseButtonReleased:
+            event = this->buildMouseButtonReleasedEvent(e);
             break;
         default:
             break;
@@ -26,49 +36,50 @@ const Event& SFMLEventBuilder::build(sf::Event* rawEvent) {
 }
 
 Event* SFMLEventBuilder::buildClosedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
-
-    // return event;
+    Event* event = new event::ClosedEvent();
+    return event;
 }
 
 Event* SFMLEventBuilder::buildResizedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
+    //Event* event = new
 
     // return event;
 }
 
 Event* SFMLEventBuilder::buildKeyPressedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
-
-    // return event;
+    event::KeyboardEvent* event = new event::KeyboardEvent();
+    event->setState(keyboard::Pressed);
+    //event->setKey(keyboard::Key k);
+    return static_cast<Event*>(event);
 }
 
 Event* SFMLEventBuilder::buildKeyReleasedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
-
-    // return event;
+    event::KeyboardEvent* event = new event::KeyboardEvent();
+    event->setState(keyboard::Released);
+    //event->setKey(keyboard::Key k);
+    return static_cast<Event*>(event);
 }
 
 Event* SFMLEventBuilder::buildMouseMovedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
+    //Event* event = new
 
     // return event;
 }
 
 Event* SFMLEventBuilder::buildMouseButtonPressedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
+    //Event* event = new
 
     // return event;
 }
 
 Event* SFMLEventBuilder::buildMouseButtonReleasedEvent(sf::Event* rawEvent) {
-    //Event* event = new 
+    //Event* event = new
 
     // return event;
 }
 
 Event* SFMLEventBuilder::buildDefaultEvent(sf::Event* rawEvent) {
-    //Event* event = new 
+    //Event* event = new
 
     // return event;
 }
