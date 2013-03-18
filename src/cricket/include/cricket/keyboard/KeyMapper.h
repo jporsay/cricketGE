049f2945b1a97edf8d50fd92d3d734b5f18306a4
Juan Pablo Orsay <jporsay@gmail.com>
2013-03-17 22:46:46 -0300

    changed the way key mappers work

--- src/cricket/include/cricket/keyboard/KeyMapper.h
+++ src/cricket/include/cricket/keyboard/KeyMapper.h
@@ -1,30 +1,16 @@
 #pragma once
 
 #include <map>
-
+#include "Keys.h"
 namespace keyboard {
 
 using namespace std;
 
-template<typename T, typename J>
+template<typename T>
 class KeyMapper {
-private:
-    map<T, J> mapping;
+
 public:
-    virtual void initMapper() = 0;
-
-    void addMapping(T key, J value) {
-        mapping.insert(pair<T, J>(key, value));
-    }
-
-    J getValue(T key) {
-        typename map<T, J>::iterator it = mapping.find(key);
-        if (it != mapping.end()) {
-            return *it;
-        } else {
-            return 0;
-        }
-    }
+    virtual const Key getKey(T key) const = 0;
 };
 
 }
