c5f82cc6bfbf018de8df14ec08f37ec9f194ba70
Juan Pablo Orsay <jporsay@gmail.com>
2013-03-17 22:49:04 -0300

    listening to keyboard event

--- src/game/AsteroidsGame.cpp
+++ src/game/AsteroidsGame.cpp
@@ -5,11 +5,15 @@
 #include <cricket/event/EventType.h>
 #include <cricket/event/Service.h>
 #include <cricket/event/ClosedEvent.h>
+#include <cricket/event/KeyboardEvent.h>
+
+#include <iostream>
 
 bool AsteroidsGame::initialize() {
     setWindow(new window::SFMLWindow(800, 600, "Cricket GE"));
     getWindow()->initialize();
     event::Service::get().subscribe(event::EventType::CLOSE, this);
+    event::Service::get().subscribe(event::EventType::KEYBOARD, this);
     return true;
 }
 
@@ -27,8 +31,18 @@ void AsteroidsGame::shutDown() {
 }
 
 bool AsteroidsGame::notify(const event::Event& e) {
-    if (e.isOfType(event::EventType::CLOSE)) {
-        shutDown();
+    switch (e.getType()) {
+        case event::EventType::CLOSE:
+            shutDown();
+        break;
+        case event::EventType::KEYBOARD: {
+            const event::KeyboardEvent* ev = static_cast<const event::KeyboardEvent*>(&e);
+            if (ev->getState() == keyboard::Pressed) {
+                std::cout << "Pressed key: " << ev->getKey() << std::endl;
+            }
+        }
+        break;
+        default: break;
     }
     return true;
 }
